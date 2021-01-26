3a7d2da7e980bdb43891c3b4f70cfaa0eb779d64
Augustin Leconte <augustin.leconte@epitech.eu>
2021-01-26 17:17:56 +0100

    add chat UI

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-message',
@@ -7,15 +8,17 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class MessageComponent implements OnInit {
 
+	user : any;
 	@Input() author: any;
 	@Input() content: string;
 	@Input() reaction: number;
 	@Input() created_at: string;
 	@Input() origin: string;
 
-  constructor() { }
+	constructor(private authService: AuthService) {}
 
-  ngOnInit(): void {
-  }
-
+	ngOnInit() {
+		console.log(this.authService.getUser());
+		this.user = this.authService.user;
+	}
 }
